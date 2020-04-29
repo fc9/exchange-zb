8a96352e30051805491d5324b71327a553c68be7
fc9 <fabiocabralsantos@gmail.com>
2020-04-29 12:15:37 -0300

    update mail

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -16,7 +16,7 @@
 		<tbody style="display:block; width:100%">
 			<tr style="display:block; width:100%">
 				<td style="min-width: 660px">
-					<img style="width:240px; display:block; position: relative; margin: 24px auto" src="https://maisvoz.com.br/theme/logo-black.png" alt="Mais Voz" class="CToWUd">
+					<img style="width:240px; display:block; position: relative; margin: 24px auto" src="http://onebitx.com/theme/logo.svg" alt="Mais Voz" class="CToWUd">
 					<h1 style="font-size:25px; line-height:30px; font-weight:400; margin:50px 0 25px 0; padding: 0 18px 0; color:#202638; font-family:sans-serif">{{ $subject }}</h1>
 				    <code style="padding: 0 18px 0; font-size:12px; font-weight:200; font-family:sans-serif; line-height:24px; margin:0 0 15px 0">
 						<b>{{ $name }} ({{ $email }})</b>
@@ -25,7 +25,6 @@
 					<p style="padding: 0 18px 0; font-size:16px; font-weight:200; font-family:sans-serif; line-height:24px; margin:0 0 15px 0;font-style: italic">
 						{{ $body }}
 					</p>
-				    {{--<img style="width: 100%; height: auto;" src="https://leasingback.com/images/imagem-email.jpg">--}}
 				</td>
 			</tr>
 		</tbody>
